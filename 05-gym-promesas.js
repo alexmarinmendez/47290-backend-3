c2adf171ac6d198728a45491c266d1220c8de1b4
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-07-26 19:58:13 -0500

    feat: ejemplos encadenamiento de promesas

--- 05-gym-promesas.js
+++ 05-gym-promesas.js
@@ -1,12 +1,19 @@
 const pA = new Promise(function(resolve, reject) {
-    // resolve('Resuelve al valor de A')
-    reject('Error en A')
+    resolve('Resuelve al valor de A')
+    // reject('Error en A')
 })
 
-pA.then(null, function(err) {
-    console.log(err)
-})
-
-pA.catch(function(err) {
-    console.log(err)
-})
+pA
+    .then()
+    .then()
+    .then(function(value) {
+        console.log(value)
+        throw new Error('Error nuevo 1')
+    })
+    .then(function(value) {
+        console.log(value)
+        throw new Error('Error nuevo 2')
+    })
+    .then(null, function(err) {
+        console.log(err)
+    })
